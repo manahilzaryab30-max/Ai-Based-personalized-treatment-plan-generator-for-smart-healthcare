2b801c68abc2a29f07efd88b7656787ab383eef3
manahilzaryab30-max <manahilzaryab30@gmail.com>
2026-09-03 13:30:08 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,2 @@
 # Ai-Based-personalized-treatment-plan-generator-for-smart-healthcare
-# AI-based personalized treatment plan generator using machine learning to predict diseases and provide personalized treatment and follow-up recommendations.
+# AI-based personalized treatment plan generator.
